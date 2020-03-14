45997bb3dd3257d9ef306071564c179bbd581eba
Li-Hsuan <lihsuanlung@gmail.com>
2020-03-14 13:03:28 -0400

    Convert snake to camelcase for javascript convention

--- src/model.js
+++ src/model.js
@@ -3,48 +3,48 @@ class Model {
     this.options = options;
   }
 
-  static number_of_cases_in_day_1() { return 1 }
+  static numberOfCasesInDay1() { return 1 }
 
-  number_of_cases(previous_number_of_cases) {
-    return previous_number_of_cases * (2 ** (1/this.options.number_of_days_per_doubling));
+  numberOfCases(previousNumberOfCases) {
+    return previousNumberOfCases * (2 ** (1/this.options.numberOfDaysPerDoubling));
   }
 
-  number_of_deaths(number_of_cases) {
-    return number_of_cases
-      / ((2 ** (1/this.options.number_of_days_per_doubling)) ** this.options.number_of_days_from_infection_to_death)
-      * this.options.mortality_rate;
+  numberOfDeaths(numberOfCases) {
+    return numberOfCases
+      / ((2 ** (1/this.options.numberOfDaysPerDoubling)) ** this.options.numberOfDaysFromInfectionToDeath)
+      * this.options.mortalityRate;
   }
 
-  number_hospitalized(number_of_cases) {
-    return number_of_cases
-      / ((2 ** (1/this.options.number_of_days_per_doubling)) ** this.options.number_of_days_from_infection_to_hospitalization)
-      * this.options.hospitalization_rate;
+  numberHospitalized(numberOfCases) {
+    return numberOfCases
+      / ((2 ** (1/this.options.numberOfDaysPerDoubling)) ** this.options.numberOfDaysFromInfectionToHospitalization)
+      * this.options.hopistalizationRate;
   }
 
-  number_in_hospital_at_the_time(number_of_cases) {
-    return this.number_hospitalized(number_of_cases)
-      - (number_of_cases / ((2 ** (1/this.options.number_of_days_per_doubling)) ** this.options.number_of_days_from_infection_to_out_of_hospital));
+  numberInHospitalAtTheTime(numberOfCases) {
+    return this.numberHospitalized(numberOfCases)
+      - (numberOfCases / ((2 ** (1/this.options.numberOfDaysPerDoubling)) ** this.options.numberOfDaysFromInjectionToOutOfHospital));
   }
 
   ofDay(number) {
     if (number == 1) {
       return {
-        number_of_cases: Model.number_of_cases_in_day_1(),
-        number_of_deaths: this.number_of_deaths(Model.number_of_cases_in_day_1()),
-        number_hospitalized: this.number_hospitalized(Model.number_of_cases_in_day_1()),
-        number_in_hospital_at_the_time: this.number_in_hospital_at_the_time(
-          Model.number_of_cases_in_day_1(), this.number_hospitalized(Model.number_of_cases_in_day_1())),
+        numberOfCases: Model.numberOfCasesInDay1(),
+        numberOfDeaths: this.numberOfDeaths(Model.numberOfCasesInDay1()),
+        numberHospitalized: this.numberHospitalized(Model.numberOfCasesInDay1()),
+        numberInHospitalAtTheTime: this.numberInHospitalAtTheTime(
+          Model.numberOfCasesInDay1(), this.numberHospitalized(Model.numberOfCasesInDay1())),
       };
     }
 
-    const previous_number_of_cases = this.ofDay(number - 1).number_of_cases;
-    const number_of_cases = this.number_of_cases(previous_number_of_cases);
+    const previousNumberOfCases = this.ofDay(number - 1).numberOfCases;
+    const numberOfCases = this.numberOfCases(previousNumberOfCases);
 
     return {
-      number_of_cases: this.number_of_cases(previous_number_of_cases),
-      number_of_deaths: this.number_of_deaths(number_of_cases),
-      number_hospitalized: this.number_hospitalized(number_of_cases),
-      number_in_hospital_at_the_time: this.number_in_hospital_at_the_time(number_of_cases)
+      numberOfCases: this.numberOfCases(previousNumberOfCases),
+      numberOfDeaths: this.numberOfDeaths(numberOfCases),
+      numberHospitalized: this.numberHospitalized(numberOfCases),
+      numberInHospitalAtTheTime: this.numberInHospitalAtTheTime(numberOfCases)
     }
   }
 }
--- src/model.test.js
+++ src/model.test.js
@@ -3,94 +3,94 @@ const Model = require('./model');
 describe('Coronavirus Epidemiology Model', () => {
   beforeEach(() => {
     const options = {
-      number_of_days_per_doubling: 7,
-      number_of_days_from_infection_to_death: 20,
-      mortality_rate: 0.015,
-      number_of_days_from_infection_to_hospitalization: 12,
-      hospitalization_rate: 0.2,
-      number_of_days_from_infection_to_out_of_hospital: 40,
+      numberOfDaysPerDoubling: 7,
+      numberOfDaysFromInfectionToDeath: 20,
+      mortalityRate: 0.015,
+      numberOfDaysFromInfectionToHospitalization: 12,
+      hopistalizationRate: 0.2,
+      numberOfDaysFromInjectionToOutOfHospital: 40,
     };
 
     this.model = new Model(options);
   });
 
-  describe('number_of_cases', () => {
+  describe('numberOfCases', () => {
     it('for day 1', () => {
       const dataset = this.model.ofDay(1);
 
-      expect(dataset.number_of_cases).toBe(1);
+      expect(dataset.numberOfCases).toBe(1);
     });
 
     it('for day 2', () => {
       const dataset = this.model.ofDay(2);
 
-      expect(dataset.number_of_cases).toBeCloseTo(1.10, 2);
+      expect(dataset.numberOfCases).toBeCloseTo(1.10, 2);
     });
 
     it('for day 3', () => {
       const dataset = this.model.ofDay(3);
 
-      expect(dataset.number_of_cases).toBeCloseTo(1.22, 2);
+      expect(dataset.numberOfCases).toBeCloseTo(1.22, 2);
     });
   });
 
-  describe('number_of_deaths', () => {
+  describe('numberOfDeaths', () => {
     it('for day 1', () => {
       const dataset = this.model.ofDay(1);
 
-      expect(dataset.number_of_deaths).toBeCloseTo(0.00, 1);
+      expect(dataset.numberOfDeaths).toBeCloseTo(0.00, 1);
     });
 
     it('for day 10', () => {
       const dataset = this.model.ofDay(10);
 
-      expect(dataset.number_of_deaths).toBeCloseTo(0.01, 1);
+      expect(dataset.numberOfDeaths).toBeCloseTo(0.01, 1);
     });
 
     it('for day 21', () => {
       const dataset = this.model.ofDay(21);
 
-      expect(dataset.number_of_deaths).toBeCloseTo(0.02, 1);
+      expect(dataset.numberOfDeaths).toBeCloseTo(0.02, 1);
     });
   });
 
-  describe('number_hospitalized', () => {
+  describe('numberHospitalized', () => {
     it('for day 1', () => {
       const dataset = this.model.ofDay(1);
 
-      expect(dataset.number_hospitalized).toBeCloseTo(0.06, 2);
+      expect(dataset.numberHospitalized).toBeCloseTo(0.06, 2);
     });
 
     it('for day 2', () => {
       const dataset = this.model.ofDay(2);
 
-      expect(dataset.number_hospitalized).toBeCloseTo(0.07, 2);
+      expect(dataset.numberHospitalized).toBeCloseTo(0.07, 2);
     });
 
     it('for day 4', () => {
       const dataset = this.model.ofDay(4);
 
-      expect(dataset.number_hospitalized).toBeCloseTo(0.08, 2);
+      expect(dataset.numberHospitalized).toBeCloseTo(0.08, 2);
     });
   });
 
-  describe('number_in_hospital_at_the_time', () => {
+  describe('numberInHospitalAtTheTime', () => {
     it('for day 1', () => {
       const dataset = this.model.ofDay(1);
 
-      expect(dataset.number_in_hospital_at_the_time).toBeCloseTo(0.06, 1);
+      expect(dataset.numberInHospitalAtTheTime).toBeCloseTo(0.06, 1);
     });
 
     it('for day 3', () => {
       const dataset = this.model.ofDay(3);
 
-      expect(dataset.number_in_hospital_at_the_time).toBeCloseTo(0.07, 1);
+      expect(dataset.numberInHospitalAtTheTime).toBeCloseTo(0.07, 1);
     });
 
     it('for day 4', () => {
       const dataset = this.model.ofDay(4);
 
-      expect(dataset.number_in_hospital_at_the_time).toBeCloseTo(0.08, 1);
+      expect(dataset.numberInHospitalAtTheTime).toBeCloseTo(0.08, 1);
     });
   });
 });
